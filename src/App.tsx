6c701c2db2a9dbab3edb199bdb8fcad05f14ff36
Yuya757 <pantera.yk37@gmail.com>
2025-02-25 14:42:46 +0900

    wip; レスポンシブ

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { ApprovalPage } from "./pages/ApprovalPage";
 
 function App() {
-  return <ApprovalPage />;
+  return (
+    <div className="max-w-xs mx-auto px-1 sm:px-6 lg:px-8">
+      <ApprovalPage />
+    </div>
+  );
 }
 
 export default App;
